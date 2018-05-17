9e7b0157ab567f3a9c2e39f6f6c8fa29cee1fa21
nickbrenn <nbren@tutanota.com>
2018-05-17 16:34:27 -0400

    Got tokens and logging in to work

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const JwtStrategy = require("passport-jwt").Strategy;
+const { ExtractJwt } = require("passport-jwt");
 
 const User = require("./users/User");
+const secret = "SECRET?";
 
 mongoose
   .connect("mongodb://localhost/authdb")
@@ -11,13 +17,70 @@ mongoose
 
 const server = express();
 
-// function authenticate(req, res, next) {
-//   if (req.session && req.session.username) {
-//     next();
-//   } else {
-//     res.status(401).send("You shall not pass!!!");
-//   }
-// }
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        done(null, false);
+      } else {
+        user
+          .comparePassword(password)
+          .then(isValid => {
+            if (isValid) {
+              const { _id, username } = user;
+              return done(null, { _id, username });
+            } else {
+              return done(null, false);
+            }
+          })
+          .catch(err => {
+            return done(err);
+          });
+      }
+    })
+    .catch(err => done(err));
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub)
+    .then(user => {
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    })
+    .catch(err => {
+      done(err);
+    });
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+const passportOptions = { session: false };
+const authenticate = passport.authenticate("local", passportOptions);
+const protected = passport.authenticate("jwt", passportOptions);
+
+function makeToken(user) {
+  const timestamp = new Date().getTime();
+  const payload = {
+    sub: user._id,
+    iat: timestamp,
+    username: user.username,
+    role: user.role
+  };
+  const options = {
+    expiresIn: "24h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 server.use(helmet());
 server.use(express.json());
@@ -30,32 +93,26 @@ server.post("/register", function(req, res) {
   const user = new User(req.body);
   user
     .save()
-    .then(user => res.status(201).send(user))
-    .catch(err => res.status(500).send(err));
-});
-
-server.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  User.findOne({ username })
     .then(user => {
-      if (user) {
-        user.comparePassword(password).then(isMatch => {
-          if (isMatch) {
-            res.send("login successful");
-          } else {
-            res.status(401).send("invalid credentials");
-          }
-        });
-      } else
-        res
-          .status(404)
-          .send(`There is no user with the username ${req.body.username}.`);
+      const token = makeToken(user);
+      res.status(201).json({ user, token });
     })
-    .catch(err => res.send("LOGIN ERROR"));
+    .catch(err => res.status(500).json(err));
 });
 
-server.get("/users", authenticate, (req, res) => {
-  User.find().then(users => res.send(users));
+server.post("/login", authenticate, (req, res) => {
+  res.status(200).json({ token: makeToken(req.user), user: req.user });
+});
+
+server.get("/users", protected, (req, res) => {
+  User.find()
+    .select("username")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 const port = process.env.PORT || 5000;
